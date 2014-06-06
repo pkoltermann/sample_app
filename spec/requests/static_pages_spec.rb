7310f8c1ba7d772286820470a5e36173cdd06910
Przemyslaw Koltermann <przemek@sirlocal.pl>
2014-06-06 17:45:01 +0200

    About site & test update

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,22 +1,30 @@
 require 'spec_helper'
 
+basic_title = 'RoR Tut App'
+
 describe "Static pages" do
   describe "Home page" do
     it "should have title 'Home'" do
       visit '/static_pages/home'
-      page.should have_selector('title', :text =>'home')
+      page.should have_selector('title', :text => basic_title)
     end
   end
   describe "Help page" do
     it "should have title 'Help'" do
       visit '/static_pages/help'
-      page.should have_selector('title', :text =>'help')
+      page.should have_selector('title', :text => "#{basic_title} | help")
+    end
+  end
+  describe "About page" do
+    it "should have title 'About'" do
+      visit '/static_pages/about'
+      page.should have_selector('title', :text =>"#{basic_title} | about")
     end
   end
   describe "About page" do
     it "should have title 'Contact'" do
       visit '/static_pages/contact'
-      page.should have_selector('title', :text =>'contact')
+      page.should have_selector('title', :text =>"#{basic_title} | contact")
     end
   end
 end
