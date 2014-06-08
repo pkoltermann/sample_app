347712ba66e6d4f4d56f93e35c44d0c6f8fb9b16
Przemyslaw Koltermann <kolemp@gmail.com>
2014-06-08 19:22:28 +0200

    Setting paths

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,22 +1,58 @@
 require 'spec_helper'
 
 describe "Static pages" do
+
   describe "Home page" do
+
     it "should have title 'Home'" do
-      visit '/static_pages/home'
-      page.should have_selector('title', :text =>'home')
+      visit root_path
+      page.should have_selector('h1', :text =>'home')
     end
+
+    it "should have base title 'Home'" do
+      visit root_path
+      page.should have_selector('title', :text =>'RoR Tut App')
+    end
+
+    it "should have custom title 'Home'" do
+      visit root_path
+      page.should have_selector('title', :text =>'| home')
+    end
+
   end
   describe "Help page" do
+
     it "should have title 'Help'" do
-      visit '/static_pages/help'
-      page.should have_selector('title', :text =>'help')
+      visit help_path
+      page.should have_selector('h1', :text =>'help')
     end
+
+    it "should have base title 'Help'" do
+      visit help_path
+      page.should have_selector('title', :text =>'RoR Tut App')
+    end
+
+    it "should have custom title 'Help'" do
+      visit help_path
+      page.should have_selector('title', :text =>'| help')
+    end
+
   end
-  describe "About page" do
+
+  describe "Contact page" do
     it "should have title 'Contact'" do
-      visit '/static_pages/contact'
-      page.should have_selector('title', :text =>'contact')
+      visit contact_path
+      page.should have_selector('h1', :text =>'contact')
+    end
+
+    it "should have base title 'Contact'" do
+      visit contact_path
+      page.should have_selector('title', :text =>'RoR Tut App')
+    end
+
+    it "should have custom title 'Contact'" do
+      visit contact_path
+      page.should have_selector('title', :text =>'| contact')
     end
   end
 end
